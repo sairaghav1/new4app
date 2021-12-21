19337669c4e4ecfa03fab710652c01bfca5d7d20
Raghav <raghavb273@gmail.com>
2021-12-22 01:26:04 +0530

    js file git test

---
+++ literals.js
@@ -0,0 +1,8 @@
+const person ={
+    name:'Raghav',
+    age:22
+}
+
+const aboutMe ="My name is "+person.name+" and I am "+person.age+' years old'
+const new1 =` my name is ${person.name} and I am ${person.age} years old`
+console.log(new1)
